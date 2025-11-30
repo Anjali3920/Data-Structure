19e302b74a380bba51b5561aa14b502de80a4d19
Anjali3920 <anjali3920@gmail.com>
2025-11-30 11:06:06 +0530

    Refactor singly linked list implementation

--- 01_Singly_Linked_List.cpp
+++ 01_Singly_Linked_List.cpp
@@ -1,201 +1,213 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-  int data;
-  Node* next;
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int val = 0) {
+        data = val;
+        next = nullptr;
+    }
 };
 
- Node* head;
+Node* head = nullptr;
 
-void insertAtHead(int value){
-  Node* newNode = new Node(value);
-  newNode -> data = value;
-  newNode -> next = head;
-  head = newNode;
+void insertAtHead(int value) {
+    Node* newNode = new Node(value);
+    newNode->next = head;
+    head = newNode;
 }
 
-Node* traverse(int n){
-  Node* temp = head;
-  
-  for(int i = 1 ; i < n && temp != nullptr; i++){
-    temp = temp->next;
-  }
-  return temp;
-}
-
-void insertInBetween(int value, int i){
-  Node* temp = traverse(i);
-  if(i<=0){
-    cout << "Invalid input" << endl;
-  }
-
-  else if(temp == nullptr){
-    cout <<"Position out of bound"<< endl;
-  }
-
-  else if(i == 1){
-    insertAtHead(value);
-  }
-
-  else {
-    Node* newNode = new Node();
-    newNode -> data = value;
-    newNode -> next = temp -> next;
-    temp -> next = newNode;
-  }
-}
-
-void deleteHead(){
-  Node* temp = head;
-  head = head->next;
-  delete temp;
-}
-
-void deleteInBetween(int i){
-  if(i == 1){
-    deleteHead();
-  }
-
-  else if(head == nullptr){
-    cout << "Linked List is empty" << endl;
-  }
-
-  else {
-  Node* temp = traverse(i);
-  if(temp == nullptr || i == 0){
-    cout << "Position " << i << " does not exist" << endl;
-  }
-  else{
-  traverse(i-1)->next = temp->next;
-  delete(temp);
-  }
-  }
-}
-
-void display(){
-  Node* temp = head;
-  while(temp != nullptr){
-    cout << temp->data;
-    if(temp->next !=nullptr){
-      cout << " -> ";
+Node* traverse(int n) {
+    if (n <= 0) return nullptr;
+    
+    Node* temp = head;
+    for (int i = 1; i < n && temp != nullptr; i++) {
+        temp = temp->next;
     }
-    temp = temp->next;
-  }
-  cout << endl;
+    return temp;
 }
 
-void defaultList(){
-  head = NULL;
-  insertAtHead(5);
-  insertAtHead(4);
-  insertAtHead(3);
-  insertAtHead(2);
-  insertAtHead(1);
-
-  cout<< "Default Singly linked list: " << endl;
-  display();
-}
-
-Node* search(int n){
-  Node* temp = head;
-
-  while( temp != nullptr){
-    if(temp->data == n){
-      cout << "Pointer Of '" << n << "' is " << temp <<endl;
-      return temp;
+void insertInBetween(int value, int i) {
+    if (i <= 0) {
+        cout << "Invalid input" << endl;
+        return;
     }
-    else{
-    temp = temp->next;
+    
+    if (i == 1) {
+        insertAtHead(value);
+        return;
     }
-  }
-
-  cout << "No Match Found" << endl;
-  return nullptr;  
+    
+    Node* temp = traverse(i - 1);
+    if (temp == nullptr) {
+        cout << "Position out of bound" << endl;
+        return;
+    }
+    
+    Node* newNode = new Node(value);
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
-void reverse(){
-  Node* current , *prev, *next;
-  current = head;
-  prev = nullptr;
-
-  while (current != nullptr){
-    next = current->next;
-    current->next = prev;
-    prev = current;
-    current = next;
-  }
-
-  head = prev;
-
+void deleteHead() {
+    if (head == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    Node* temp = head;
+    head = head->next;
+    delete temp;
 }
 
-void choice(){
-  int n;
-  cout<< "Chose on of the Following Option" << endl;
-  cout<< "1. Insert at Head" << endl;
-  cout<< "2. Insert at ith position" <<endl;
-  cout<< "3. Delete at Head" << endl;
-  cout<< "4. Delete at ith position" <<endl;
-  cout<< "5. Search for an element x in the singly linked list and return its pointer" << endl;
-  cout<< "6. Reverse the Linked List" <<endl;
-  cin>> n;
-
-  if (n == 1){
-    int x;
-    cout << "Enter the element you want to insert at Head"<< endl;
-    cin >> x;
-    insertAtHead(x);
-  }
-  if (n == 2){
-    int x, i;
-    cout << "Enter the element you want to insert"<< endl;
-    cin >> x;
-    cout << "Enter the Position"<< endl;
-    cin >> i;
-    insertInBetween(x,i);
-  }
-  if (n == 3){
-    deleteHead();
-    cout << "Element at head deleted"<< endl;
-  }
-  if (n == 4){
-    int i;
-    cout << "Enter the Position you want to delete"<< endl;
-    cin >> i;
-
-    deleteInBetween(i);
-  }
-  if (n == 5){
-    int x;
-    cout << "Enter the element you want to search"<< endl;
-    cin >> x;
-
-    search(x);
-
-  }
-
-  if (n == 6){
-    reverse();
-  }
-
-  else if(n>6 || n<1){
-    cout << "Invalid Option" << endl;
-  }
+void deleteInBetween(int i) {
+    if (i <= 0) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    
+    if (i == 1) {
+        deleteHead();
+        return;
+    }
+    
+    Node* prev = traverse(i - 1);
+    if (prev == nullptr || prev->next == nullptr) {
+        cout << "Position " << i << " does not exist" << endl;
+        return;
+    }
+    
+    Node* temp = prev->next;
+    prev->next = temp->next;
+    delete temp;
 }
 
-
-
-int main(){
-  cout << "**********************************************************************************" << endl;
-  defaultList();
-  cout << "**********************************************************************************" << endl;
-  
-  choice();
-
-  cout<< "Singly linked list:  ";
-  display();
-  cout << endl;
-
-  return 0;
+void display() {
+    if (head == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data;
+        if (temp->next != nullptr) {
+            cout << " -> ";
+        }
+        temp = temp->next;
+    }
+    cout << endl;
 }
+
+void defaultList() {
+    head = nullptr;
+    insertAtHead(5);
+    insertAtHead(4);
+    insertAtHead(3);
+    insertAtHead(2);
+    insertAtHead(1);
+    
+    cout << "Default Singly linked list: " << endl;
+    display();
+}
+
+Node* search(int n) {
+    Node* temp = head;
+    
+    while (temp != nullptr) {
+        if (temp->data == n) {
+            cout << "Pointer Of '" << n << "' is " << temp << endl;
+            return temp;
+        }
+        temp = temp->next;
+    }
+    
+    cout << "No Match Found" << endl;
+    return nullptr;
+}
+
+void reverse() {
+    if (head == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    
+    Node* current = head;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+    
+    while (current != nullptr) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    
+    head = prev;
+    cout << "List reversed successfully" << endl;
+}
+
+void choice() {
+    int n;
+    cout << "\nChoose one of the Following Options" << endl;
+    cout << "1. Insert at Head" << endl;
+    cout << "2. Insert at ith position" << endl;
+    cout << "3. Delete at Head" << endl;
+    cout << "4. Delete at ith position" << endl;
+    cout << "5. Search for an element" << endl;
+    cout << "6. Reverse the Linked List" << endl;
+    cin >> n;
+    
+    if (n == 1) {
+        int x;
+        cout << "Enter the element to insert at Head: ";
+        cin >> x;
+        insertAtHead(x);
+    }
+    else if (n == 2) {
+        int x, i;
+        cout << "Enter the element to insert: ";
+        cin >> x;
+        cout << "Enter the Position: ";
+        cin >> i;
+        insertInBetween(x, i);
+    }
+    else if (n == 3) {
+        deleteHead();
+        cout << "Element at head deleted" << endl;
+    }
+    else if (n == 4) {
+        int i;
+        cout << "Enter the Position to delete: ";
+        cin >> i;
+        deleteInBetween(i);
+    }
+    else if (n == 5) {
+        int x;
+        cout << "Enter the element to search: ";
+        cin >> x;
+        search(x);
+    }
+    else if (n == 6) {
+        reverse();
+    }
+    else {
+        cout << "Invalid Option" << endl;
+    }
+}
+
+int main() {
+    cout << "**********************************************************************************" << endl;
+    defaultList();
+    cout << "**********************************************************************************" << endl;
+    
+    choice();
+    
+    cout << "Singly linked list: ";
+    display();
+    cout << endl;
+    
+    return 0;
+}
+
